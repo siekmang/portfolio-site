8c4ecd283daae51bdc8f88df0b8030bd1154c723
Greg <github@siekmang.com>
2026-07-06 13:58:22 -0400

    Reinitializing astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
-import svelte from '@astrojs/svelte';
-
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
-  integrations: [svelte()],
   adapter: cloudflare()
 });
